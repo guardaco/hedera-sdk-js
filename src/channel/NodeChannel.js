1838bf4a252d9d303bec4f3e23de7d9091f9fad8
Daniel Akhterov <daniel@launchbadge.com>
2021-09-22 19:26:32 -0700

    fix: store cert locally
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- src/channel/NodeChannel.js
+++ src/channel/NodeChannel.js
@@ -28,18 +28,33 @@ export default class NodeChannel extends Channel {
 
         let security;
 
-        // if (cert != null) {
-            // security = credentials.createSsl(Buffer.from(cert));
-            security = credentials.createSsl(null, null, null, {
-                checkServerIdentity: (_, __) => {
 
-                    console.log("fffffffffffffffffffffffffffffffffffff");
-                    return undefined;
-                }
-            });
-        // } else {
-        //     security = credentials.createInsecure();
-        // }
+        const realCert = `-----BEGIN CERTIFICATE-----
+MIICnzCCAiWgAwIBAgIUenyqJ4UaFBbwokatcUqAwW3o3rswCgYIKoZIzj0EAwMw
+gYQxCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJUWDETMBEGA1UEBwwKUmljaGFyZHNv
+bjEPMA0GA1UECgwGSGVkZXJhMQ8wDQYDVQQLDAZIZWRlcmExEDAOBgNVBAMMBzAw
+MDAwMDAxHzAdBgkqhkiG9w0BCQEWEGFkbWluQGhlZGVyYS5jb20wIBcNMjEwODIz
+MjIyMTU4WhgPMjI5NTA2MDcyMjIxNThaMIGEMQswCQYDVQQGEwJVUzELMAkGA1UE
+CAwCVFgxEzARBgNVBAcMClJpY2hhcmRzb24xDzANBgNVBAoMBkhlZGVyYTEPMA0G
+A1UECwwGSGVkZXJhMRAwDgYDVQQDDAcwMDAwMDAwMR8wHQYJKoZIhvcNAQkBFhBh
+ZG1pbkBoZWRlcmEuY29tMHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEm5b1+oG9R0qt
+zM7UZnS5l/xxUNHIHq5+NAvtlviCpJL19jrW9+/UOy00Qqc6vS6tS1hS+dNJmpiZ
+FN0EHew4VDR7ACnL4LDJKmIHWjQ0iwvZo5kCpO0r9BtPN5FvaSxyo1QwUjAPBgNV
+HREECDAGhwR/AAABMAsGA1UdDwQEAwIEsDATBgNVHSUEDDAKBggrBgEFBQcDATAd
+BgNVHQ4EFgQUeciBviJtjeuue0GPf1xllNw7qvYwCgYIKoZIzj0EAwMDaAAwZQIw
+JeG0H2HdsI1VhOYmJmYlNeKCNgAk+LMorzPmsIInVBO2HK2IrKfpReWDS/m5j51V
+AjEAxKBxDezJDqAZHTkTXCg+X9Q9V6J6M5yDy5IS90aCWEo+W8C1Hc6hkn2/NrvT
+PhwK
+-----END CERTIFICATE-----
+        `;
+
+        security = credentials.createSsl(Buffer.from(realCert));
+        // security = credentials.createSsl(Buffer.from("a"), null, null, {
+        //     checkServerIdentity: () => {
+        //         console.log("inside checkServerIdentity")
+        //         return undefined;
+        //     },
+        // });
 
         /**
          * @type {Client}
@@ -48,8 +63,13 @@ export default class NodeChannel extends Channel {
         this._client = new Client(address, security, {
             "grpc.ssl_target_name_override": "127.0.0.1",
             "grpc.default_authority": "127.0.0.1",
-            "grpc.http_connect_creds": "0",
         });
+
+        // /**
+        //  * @type {Client}
+        //  * @private
+        //  */
+        // this._client = new Client(address, credentials.createInsecure());
     }
 
     /**
@@ -59,32 +79,33 @@ export default class NodeChannel extends Channel {
      * @returns {Promise<NodeChannel>}
      */
     static async new(address, certHash) {
-        let cert = undefined;
-        if (address.endsWith(":50212") || address.endsWith(":433")) {
-            // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
-            const response = tls.connect({
-                host: "0.previewnet.hedera.com",
-                rejectUnauthorized: false,
-                ciphers: "ALL",
-                port: 50212,
-                checkServerIdentity: (_, __) => {
-                    console.log("gggggggggggggggggggggggggggggggggg");
-                    return undefined;
-                }
-            }, () => console.log("CONNNECTED -------------------------------------------------"));
-
-            await new Promise((resolved) => setTimeout(resolved, 1000));
-
-            console.log(response.getPeerCertificate(true));
-            console.log(response.getCertificate());
-            // console.log(response.getX509Certificate());
-
-            // const response = new Promise((reject, resolve) => {
-            //     https.get(options, (res) => {
-            //         res.socket.connect.getPeerCertificate();
-            //     });
-            // });
-        }
+//         let cert = undefined;
+//         if (address.endsWith(":50212") || address.endsWith(":433")) {
+//             // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+//             const response = tls.connect({
+//                 host: "0.previewnet.hedera.com",
+//                 rejectUnauthorized: false,
+//                 ciphers: "ALL",
+//                 port: 50211,
+//                 checkServerIdentity: (_, __) => {
+//                     console.log("gggggggggggggggggggggggggggggggggg");
+//                     return undefined;
+//                 }
+//             }, () => console.log("SecureConnectionListener callback called"));
+// 
+//             await new Promise((resolved) => setTimeout(resolved, 1000));
+// 
+//             console.log("Peer Certificate:", response.getPeerCertificate(true));
+//             console.log("Certificate:", response.getCertificate());
+// 
+//             // console.log(response.getX509Certificate());
+// 
+//             // const response = new Promise((reject, resolve) => {
+//             //     https.get(options, (res) => {
+//             //         res.socket.connect.getPeerCertificate();
+//             //     });
+//             // });
+//         }
 
         return new NodeChannel(address, certHash, new Uint8Array());
     }
@@ -105,7 +126,6 @@ export default class NodeChannel extends Channel {
      */
     _createUnaryClient(serviceName) {
         return (method, requestData, callback) => {
-            console.log('eeeeeeeeeeeeeeeeeeee');
             this._client.makeUnaryRequest(
                 `/proto.${serviceName}/${method.name}`,
                 (value) => value,
